skurfuerst/TYPO3.TYPO3CR

commit 773160cbd25847049a8b57a35a3e3cfb96651be1

[TASK] Add setRemoved method to node domain object
This is needed for generic method setters like the ExtDirect service

Change-Id: I67756ab05eb2e002067e06ea2dfcaaaec150d3eb

Classes/Domain/Model/Node.php
@@ -680,6 +680,18 @@ class Node implements NodeInterface {
 	}
 
 	/**
+	 * Enables using the remove method when only setters are available
+	 *
+	 * @param boolean $removed If TRUE, this node and it's child nodes will be removed
+	 * @return void
+	 */
+	public function setRemoved($removed) {
+		if ((boolean)$removed === TRUE) {
+			$this->remove();
+		}
+	}
+
+	/**
 	 * If this node is a removed node.
 	 *
 	 * @return boolean

commit 7b970f812b2e70dcd8a86cd0b51a022764619d76

[BUGFIX] Fix setRemoved() for proxy nodes
The method Node::setRemoved() has not been implemented for ProxyNode
and thus nodes which are proxy nodes, are not marked as removed when
using that method.

This patch also introduces tests for that part and a new LoggingAspect
which - for now - only logs removal of nodes.

This bug has been introduced in 773160cbd25847049a8b57a35a3e3cfb96651be1

Change-Id: I903e144d037de9698579ee68c634a517f0f0eaa0
Related: #40754

Classes/Aspect/LoggingAspect.php
@@ -1,0 +1,44 @@
+<?php
+namespace TYPO3\TYPO3CR\Aspect;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "TYPO3CR".                    *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * An aspect which centralizes the logging of node operations
+ *
+ * @FLOW3\Aspect
+ * @FLOW3\Scope("singleton")
+ */
+class LoggingAspect {
+
+	/**
+	 * @var \TYPO3\FLOW3\Log\SystemLoggerInterface
+	 * @FLOW3\Inject
+	 */
+	protected $systemLogger;
+
+	/**
+	 * Logs calls of remove()
+	 *
+	 * @FLOW3\AfterReturning("method(TYPO3\TYPO3CR\Domain\Model\Node->remove())")
+	 * @param \TYPO3\FLOW3\Aop\JoinPointInterface $joinPoint The current joinpoint
+	 * @return mixed The result of the target method if it has not been intercepted
+	 */
+	public function logRemove(\TYPO3\FLOW3\Aop\JoinPointInterface $joinPoint) {
+		$node = $joinPoint->getProxy();
+		$this->systemLogger->log(sprintf('Removed node "%s" from workspace "%s" (identifier: "%s")', $node->getPath(), $node->getWorkspace()->getName(), $node->getIdentifier()), LOG_DEBUG);
+	}
+
+}
+
+?>

Classes/Domain/Model/Node.php
@@ -843,7 +843,7 @@ class Node implements NodeInterface {
 	/**
 	 * Enables using the remove method when only setters are available
 	 *
-	 * @param boolean $removed If TRUE, this node and it's child nodes will be removed
+	 * @param boolean $removed If TRUE, this node and it's child nodes will be removed. Cannot handle FALSE (yet).
 	 * @return void
 	 */
 	public function setRemoved($removed) {

Classes/Domain/Model/NodeInterface.php
@@ -377,6 +377,14 @@ interface NodeInterface {
 	public function remove();
 
 	/**
+	 * Enables using the remove method when only setters are available.
+	 *
+	 * @param boolean $removed If TRUE, this node and it's child nodes will be removed. Cannot handle FALSE (yet).
+	 * @return void
+	 */
+	public function setRemoved($removed);
+
+	/**
 	 * If this node is a removed node.
 	 *
 	 * @return boolean

Classes/Domain/Model/ProxyNode.php
@@ -544,6 +544,18 @@ class ProxyNode implements NodeInterface {
 	}
 
 	/**
+	 * Enables using the remove method when only setters are available
+	 *
+	 * @param boolean $removed If TRUE, this node and it's child nodes will be removed. Cannot handle FALSE (yet).
+	 * @return void
+	 */
+	public function setRemoved($removed) {
+		if ((boolean)$removed === TRUE) {
+			$this->remove();
+		}
+	}
+
+	/**
 	 * If this node is a removed node.
 	 *
 	 * @return boolean

Tests/Unit/Domain/Model/NodeTest.php
@@ -518,6 +518,15 @@ class NodeTest extends \TYPO3\FLOW3\Tests\UnitTestCase {
 
 	/**
 	 * @test
+	 */
+	public function setRemovedCallsRemoveMethodIfArgumentIsTrue() {
+		$node = $this->getAccessibleMock('TYPO3\TYPO3CR\Domain\Model\Node', array('remove'), array(), '', FALSE);
+		$node->expects($this->once())->method('remove');
+		$node->setRemoved(TRUE);
+	}
+
+	/**
+	 * @test
 	 * @dataProvider abnormalPaths
 	 */
 	public function normalizePathReturnsANormalizedAbsolutePath($currentPath, $relativePath, $normalizedPath) {

Tests/Unit/Domain/Model/ProxyNodeTest.php
@@ -13,7 +13,6 @@ namespace TYPO3\TYPO3CR\Tests\Unit\Domain\Model;
 
 /**
  * Testcase for the "ProxyNode" domain model
- *
  */
 class ProxyNodeTest extends \TYPO3\FLOW3\Tests\UnitTestCase {
 
@@ -287,8 +286,10 @@ class ProxyNodeTest extends \TYPO3\FLOW3\Tests\UnitTestCase {
 	/**
 	 * @test
 	 */
-	public function getLabelCallsGetLabelOnTheNewNodeOrTheOriginalNode() {
-		$this->assertThatOriginalOrNewNodeIsCalled('getLabel');
+	public function setRemovedCallsRemoveMethodIfArgumentIsTrue() {
+		$node = $this->getAccessibleMock('TYPO3\TYPO3CR\Domain\Model\ProxyNode', array('remove'), array(), '', FALSE);
+		$node->expects($this->once())->method('remove');
+		$node->setRemoved(TRUE);
 	}
 
 	/**